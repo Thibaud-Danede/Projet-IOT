c880930dbf708f690739c0f5646057d5f0123424
Thibaud-Danede <151736283+Thibaud-Danede@users.noreply.github.com>
2025-11-17 14:19:54 +0100

    Fix: correctif sur l'IHM

--- .PROJECT/ESP-D1_Project/ESP-D1_Project.ino
+++ .PROJECT/ESP-D1_Project/ESP-D1_Project.ino
@@ -10,17 +10,33 @@
 
 #include "ESP-D1_GPIO.h"
 
+#include "DHT.h"
+
+#define DHTTYPE DHT11
+
 /* ---------------- PINS ---------------- */
 const int LED_PIN = D5;
 const int BUTTON_PIN = D7;
+const int DHT_PIN = D4;
 
 /* ---------------- ETATS ---------------- */
 int buttonState = LOW;
 int ledState = LOW;
+int dhtState = LOW;
+
+/* -------------- DHT OBJECT -------------- */
+float humidity;
+float temperature;
+float heatIndex;
+
+/* -------- IINITIALIZE DHT SENSOR -------- */
+DHT dht(DHT_PIN, DHTTYPE);
 
 /* ---------------- TIMERS ---------------- */
 long previousMillisBtn = 0;
+long previousMillisDHT = 0;
 long buttonInterval = 500;
+long dhtInterval = 500;
 
 /* ---------------- SERIAL ---------------- */
 String inputString = "";
@@ -29,7 +45,8 @@ bool stringComplete = false;
 /* ---------------- MENU ---------------- */
 enum MenuState {
   MENU_MAIN,
-  MENU_BUTTON
+  MENU_BUTTON,
+  MENU_DHT
 };
 
 MenuState menu = MENU_MAIN;
@@ -41,6 +58,12 @@ MenuState menu = MENU_MAIN;
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, ledState);
+
+  // pinMode(DHT_PIN, OUTPUT);
+  // digitalWrite(DHT_PIN, dhtState);
+
+  // Start DHT
+  dht.begin();
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
@@ -58,7 +81,7 @@ void showMainMenu() {
   Serial.println("\n======== MENU PRINCIPAL ========");
   Serial.println("Tapez dans l'invite de commande le paramètre que vous voulez régler");
   Serial.println("1 : Gérer l'acquisition du bouton");
-  Serial.println("2 : (option future)");
+  Serial.println("2 : Gérer l'acquisition du dht");
 }
 
 void showButtonMenu() {
@@ -67,6 +90,14 @@ void showButtonMenu() {
   Serial.println("Commandes disponibles :");
   Serial.println("  BUTTONSET <ms>   : changer intervalle lecture");
   Serial.println("  RETURN           : retour au menu principal\n");
+}
+
+void showDHTMenu() {
+  Serial.println("\n=== MENU DHT ===");
+  Serial.println("Affichage des valeurs du dht...");
+  Serial.println("Commandes disponibles :");
+  Serial.println("  DHTSET <ms>   : changer intervalle lecture");
+  Serial.println("  RETURN        : retour au menu principal\n");
 }
 
 
@@ -80,6 +111,9 @@ void loop() {
     doEvery(buttonInterval, &previousMillisBtn, readButton);
   }
 
+  if (menu == MENU_DHT) {
+    doEvery(dhtInterval, &previousMillisDHT, readDHT);
+  }
   // Lecture série
   serialEvent();
 
@@ -111,6 +145,32 @@ void readButton() {
   Serial.println(buttonState);
 }
 
+/* =======================================================================
+   Fonction lecture dht
+   ======================================================================= */
+void readDHT() {
+
+  humidity = dht.readHumidity();
+
+  temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+  }
+  else {
+    // Compute heat index in Celsius (isFahreheit = false)
+    heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+  
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(temperature);
+    Serial.print(F("°C "));
+    Serial.print(F("  Heat index: "));
+    Serial.println(heatIndex);
+  }
+}
+
 
 /* =======================================================================
    Analyse des commandes utilisateur
@@ -129,8 +189,9 @@ void parseCommand(String cmd) {
     }
 
     if (cmd == "2") {
-      Serial.println("Option 2 non encore implémentée.");
-      showMainMenu();
+      //Serial.println("Option 2 non encore implémentée.");
+      menu = MENU_DHT;
+      showDHTMenu();
       return;
     }
 
@@ -167,7 +228,39 @@ void parseCommand(String cmd) {
       }
       return;
     }
+    //Réponse à l'ereur
+    Serial.println("Commande inconnue. Tapez RETURN pour revenir au menu principal.");
+    return;
+  }
 
+    /* === COMMANDE MENU DHT === */
+  if (menu == MENU_DHT) {
+
+    // Retour au menu principal
+    if (cmd == "RETURN") {
+      menu = MENU_MAIN;
+      showMainMenu();
+      return;
+    }
+
+    // BUTTONSET <ms>
+    if (cmd.startsWith("DHTSET")) {
+
+      String valueStr = cmd.substring(6);
+      valueStr.trim();
+      long newInterval = valueStr.toInt();
+
+      if (newInterval > 0) {
+        dhtInterval = newInterval;
+        Serial.print("Nouvel intervalle = ");
+        Serial.print(dhtInterval);
+        Serial.println(" ms");
+      } else {
+        Serial.println("Erreur: donnée non valide.");
+      }
+      return;
+    }
+    //Réponse à l'ereur
     Serial.println("Commande inconnue. Tapez RETURN pour revenir au menu principal.");
     return;
   }
